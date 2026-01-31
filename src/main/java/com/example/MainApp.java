5a0062f27ecc1259bc22c45ce27cd5033923e1cb
revatigithubmy <revatishimpi1955@gmail.com>
2026-02-01 00:10:03 +0530

    feat: initial spring project setup

---
+++ src/main/java/com/example/MainApp.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.example.config.AppConfig;
+import com.example.controller.UserController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        UserController controller = context.getBean(UserController.class);
+        controller.createUser("Revati");
+        controller.listUsers();
+    }
+}
